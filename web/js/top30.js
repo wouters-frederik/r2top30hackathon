c80484c9ffc93dfe3bf23d4220ce561268659140
Frederik Wouters <woutersf@gmail.com>
2017-06-01 11:05:48 +0200

    Form field names
    Signed-off-by: Frederik Wouters <woutersf@gmail.com>

--- web/js/top30.js
+++ web/js/top30.js
@@ -2,9 +2,12 @@ jQuery(document).ready(function($){
 
     $('#mainform').formToWizard({
         submitButton: '#submitbutton'
+
     });
 
+    console.log('koekoek');
     $('.input1').change(function(){
+        console.log('inp');
         $('.inputname').html($('.input1').val());
     })
 });
