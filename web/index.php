6d3ba8e670a78777ce91af13fc2bd249d26ae6d5
hansdreesen <hansdreesen@gmail.com>
2017-06-01 17:56:09 +0200

    Added description + general share image

--- web/index.php
+++ web/index.php
@@ -10,11 +10,11 @@ header("Pragma: no-cache");
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>R2Hackathon</title>
+		<title>Ontdek jouw Radio 2 Top 30 geboorteplaat</title>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="description" content="R2Hackathon">
-    <meta name="og:image" content="/img/FB-share-img.jpg">
+    <meta name="description" content="Welk nummer stond op 1 in de Radio 2 Top 30 op de dag dat jij geboren bent?">
+    <meta name="og:image" content="/img/FB-share-img-general.jpg">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 		<link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
