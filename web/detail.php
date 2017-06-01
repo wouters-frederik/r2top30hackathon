ef8510ad3a2455a7298d90aee16d14d13ad8ef8e
Frederik Wouters <woutersf@gmail.com>
2017-06-01 10:30:44 +0200

    detailpagina

--- web/detail.php
+++ web/detail.php
@@ -10,7 +10,8 @@ foreach ($dbh->query($sql) as $row) {
 }
 
 if ($user == false) {
-  echo 'oei die persoon hebben we niet gevonden. <a href="index.php">Proef zelf een svan ons spelletje.</a>';
+  echo 'oei die persoon hebben we niet gevonden. <a href="index.php">Proef zelf eens van ons spelletje.</a>';
+  die();
 }
 $user['birthday'] = date('d / m / Y',$user['geboortedatum']);
 //no id = Oei, probeer het zelf eens
@@ -18,6 +19,7 @@ $user['birthday'] = date('d / m / Y',$user['geboortedatum']);
 
 
 //echo Geboortedatum
+echo '<div>';
 echo 'Geboortedatum '.$user['birthday'] . '<br>';
 echo 'Name '.$user['name'] . '<br>';
 $hitlijst = getHitlijstData('lists?parent_lid=840');
@@ -27,6 +29,13 @@ $first_song = $aftellijst->songs[0];
 //var_dump($aftellijst->data->air_date);
 echo 'Op 1 in ' . $aftellijst->data->name . ' van ' . date('d m Y',$aftellijst->data->air_date) . '<br>';
 echo $first_song->title .' - ' . $first_song->name;
+echo '</div>';
+echo '<br>';
 
+
+echo '<div>';
+echo '<a href="https://www.facebook.com/sharer/sharer.php?u=/detail.php?id=1">Deel op Facebook</a>';
+echo '<a href="https://twitter.com/home?status=https%3A//master-7rqtwti-coio4nyeqlhz4.eu.platform.sh/detail.php?id=1">Deel op Twitter</a>';
+echo '</div>';
 //Load id
 // Render HTML
