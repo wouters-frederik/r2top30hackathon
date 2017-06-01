4d1bf618097f2cd9c7e0fbaae13a8746b24ac591
G-raph <info@g-raph.be>
2017-06-01 11:20:59 +0200

    R2TOP30: Maanden toegevoegd

--- web/index.php
+++ web/index.php
@@ -44,7 +44,18 @@ header("Pragma: no-cache");
                         <fieldset class="form-group">
                             <label for="maand">En in welke maand?</label>
                             <select class="form-control"  id="maand" name="maand" >
+                              <option value="1">Januari</option>
+                              <option value="2">Februari</option>
+                              <option value="3">Maart</option>
+                              <option value="4">April</option>
+                              <option value="5">Mei</option>
+                              <option value="6">Juni</option>
+                              <option value="7">Juli</option>
+                              <option value="8">Augustus</option>
+                              <option value="9">September</option>
+                              <option value="10">Oktober</option>
                               <option value="11">November</option>
+                              <option value="12">December</option>
                             </select>
                         </fieldset>
                         <fieldset class="form-group">
